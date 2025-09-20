7049b8060e5233a101bb221fa044ac11ac7abda7
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-20 14:39:26 +0100

    feat: add about main text

---
+++ app/components/about/section.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { GalleryPhoto } from '../image/gallery.vue';
+
+interface AboutSectionProps {
+    title: string;
+    content: string;
+    photos?: GalleryPhoto[];
+}
+
+const {
+    title,
+    content,
+    photos = []
+} = defineProps<AboutSectionProps>()
+</script>
+
+<template>
+    <div class="space-y-4">
+        <AppSubtitle>
+            {{ title }}
+        </AppSubtitle>
+        <AppDescription class="text-justify">
+            {{ content }}
+        </AppDescription>
+    </div>
+</template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,61 @@
+<script setup lang="ts">
+import { AppDescription } from '#components';
+import type { GalleryPhoto } from '~/components/image/gallery.vue';
+
+const userPhotos = ref<GalleryPhoto[]>([
+    { src: '/portfolio/photos/5.jpeg', tilt: 'left' },
+    { src: '/portfolio/photos/2.jpeg', tilt: null },
+    { src: '/portfolio/photos/1.jpeg', tilt: 'right' },
+])
+
+const { isMobile } = useDevice()
+
+const imgSize = computed(() => isMobile.value ? 20 : 40)
+</script>
+
 <template>
-    <WorkInProgress title="About Me" />
+    <div class="pt-10 md:pt-12 md:pb-6 md:px-12 flex flex-col gap-6">
+        <div class="w-full flex flex-wrap-reverse lg:flex-nowrap gap-6 lg:gap-0 items-center">
+            <AppSubtitle class="!font-bold mx-auto max-w-2xl w-fit text-center lg:text-left">
+                {{ $t("about.presentation") }}
+            </AppSubtitle>
+            <ImageGallery 
+                class="px-8 lg:px-2 w-full lg:w-auto"
+                :photos="userPhotos" 
+                :size="imgSize" 
+            />
+        </div>
+        <USeparator class="my-6" />
+        <div class="space-y-8">
+            <AboutSection
+                :title="$t('about.start.title')"
+                :content="$t('about.start.content')"
+            />
+            <USeparator/>
+            <AboutSection
+                :title="$t('about.education.title')"
+                :content="$t('about.education.content')"
+            />
+            <USeparator/>
+            <AboutSection
+                :title="$t('about.career.title')"
+                :content="$t('about.career.content')"
+            />
+            <USeparator/>
+            <AboutSection
+                :title="$t('about.clients.title')"
+                :content="$t('about.clients.content')"
+            />
+            <USeparator/>
+            <AboutSection
+                :title="$t('about.beyond.title')"
+                :content="$t('about.beyond.content')"
+            />
+            <USeparator/>
+            <AboutSection
+                :title="$t('about.current.title')"
+                :content="$t('about.current.content')"
+            />
+        </div>
+    </div>
 </template>
