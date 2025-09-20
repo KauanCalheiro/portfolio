1215673dc6d20c002bb08cda5009d715b78358e2
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-20 16:34:22 +0100

    feat: add fade transition component

---
+++ app/components/transition/fade.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface TransitionFadeProps {
+    mode?: 'in-out' | 'out-in' | 'default'
+}
+
+const { mode = 'out-in' } = defineProps<TransitionFadeProps>();
+</script>
+
+<template>
+    <Transition name="fade" :mode="mode || 'out-in'">
+        <slot />
+    </Transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
