974c14f3535ccd00d4845d6881fe3f3981ffce3b
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-21 21:32:20 +0100

    feat: add images in about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { AppDescription } from '#components';
 import type { GalleryPhoto } from '~/components/image/gallery.vue';
 
 const userPhotos = ref<GalleryPhoto[]>([
-    { src: '/portfolio/photos/5.jpeg', tilt: 'left' },
     { src: '/portfolio/photos/2.jpeg', tilt: null },
     { src: '/portfolio/photos/1.jpeg', tilt: 'right' },
 ])
@@ -30,31 +28,58 @@ const imgSize = computed(() => isMobile.value ? 20 : 40)
             <AboutSection
                 :title="$t('about.start.title')"
                 :content="$t('about.start.content')"
+                :photos="[
+                    { src: '/portfolio/photos/6.jpeg', tilt: 'right' },
+                    { src: '/portfolio/photos/7.jpeg', tilt: 'left' },
+                ]"
+                left
             />
             <USeparator/>
             <AboutSection
                 :title="$t('about.education.title')"
                 :content="$t('about.education.content')"
+                :photos="[
+                    { src: '/portfolio/photos/8.jpeg', tilt: 'right' },
+                    { src: '/portfolio/photos/10.jpeg', tilt: null },
+                    { src: '/portfolio/photos/9.jpeg', tilt: 'left' },
+                ]"
             />
             <USeparator/>
             <AboutSection
                 :title="$t('about.career.title')"
                 :content="$t('about.career.content')"
+                :photos="[
+                    { src: '/portfolio/photos/11.jpeg', tilt: null },
+                    { src: '/portfolio/photos/16.jpeg', tilt: 'right' },
+                ]"
+                left
             />
             <USeparator/>
             <AboutSection
                 :title="$t('about.clients.title')"
                 :content="$t('about.clients.content')"
+                :photos="[
+                    { src: '/portfolio/photos/14.jpeg', tilt: 'right' },
+                    { src: '/portfolio/photos/15.jpg', tilt: 'left' },
+                ]"
             />
             <USeparator/>
             <AboutSection
                 :title="$t('about.beyond.title')"
                 :content="$t('about.beyond.content')"
+                :photos="[
+                    { src: '/portfolio/photos/13.jpeg', tilt: 'right' },
+                    { src: '/portfolio/photos/12.jpeg', tilt: null },
+                ]"
+                left
             />
             <USeparator/>
             <AboutSection
                 :title="$t('about.current.title')"
                 :content="$t('about.current.content')"
+                :photos="[
+                    { src: '/portfolio/photos/5.jpeg', tilt: 'left' },
+                ]"
             />
         </div>
     </div>
