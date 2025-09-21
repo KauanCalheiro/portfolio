1c72456a3da0244cd1719f0df870cc2a231a278a
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-21 20:10:55 +0100

    feat: add suport for images

--- app/components/about/section.vue
+++ app/components/about/section.vue
@@ -5,22 +5,35 @@ interface AboutSectionProps {
     title: string;
     content: string;
     photos?: GalleryPhoto[];
+    left?: boolean;
 }
 
 const {
     title,
     content,
-    photos = []
+    photos = [],
+    left = false,
 } = defineProps<AboutSectionProps>()
 </script>
 
 <template>
-    <div class="space-y-4">
-        <AppSubtitle>
-            {{ title }}
-        </AppSubtitle>
-        <AppDescription class="text-justify">
-            {{ content }}
-        </AppDescription>
+    <div 
+        class="flex flex-col gap-6 lg:gap-8 lg:items-center"
+        :class="left ? 'lg:flex-row-reverse' : 'lg:flex-row'"
+    >
+        <div class="space-y-4">
+            <AppSubtitle>
+                {{ title }}
+            </AppSubtitle>
+            <AppDescription class="text-justify">
+                {{ content }}
+            </AppDescription>
+        </div>
+        <div>
+            <ImageGallery 
+                v-if="photos.length"
+                :photos="photos" 
+            />
+        </div>
     </div>
 </template>
