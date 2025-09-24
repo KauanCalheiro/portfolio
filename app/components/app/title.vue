a606c4e76a0fd7312bd6f6f6d607987aa1842afa
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-24 10:26:28 +0100

    style: change font size in md devices

--- app/components/app/title.vue
+++ app/components/app/title.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-4xl font-medium leading-tight tracking-tighter text-text-primary md:text-6xl md:leading-[64px]">
+    <h1 class="text-4xl font-medium leading-tight tracking-tighter text-text-primary md:text-5xl md:leading-[64px]">
         <slot />
     </h1>
 </template>
