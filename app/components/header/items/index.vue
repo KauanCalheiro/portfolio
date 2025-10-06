e858518af318114cfbd2aef4e0def54105cd7176
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-10-06 15:05:29 +0100

    refactor: reorder items

--- app/components/header/items/index.vue
+++ app/components/header/items/index.vue
@@ -1,48 +1,48 @@
 <script setup lang="ts">
 import { HeaderItemsDesktop, HeaderItemsMobile } from '#components';
 
-const { isDesktop } = useDevice()
+const { isLargeDesktop } = useDevice()
 
 const items = ref([
     {
-        label: computed( () => $t('header.home')),
+        label: computed(() => $t('header.home')),
         icon: 'mdi:home',
         to: '/'
     },
     {
-        label: computed( () => $t('header.about')),
+        label: computed(() => $t('header.about')),
         icon: 'mdi:information',
         to: '/about'
     },
     {
-        label: computed( () => $t('header.projects')),
-        icon: 'mdi:briefcase',
-        to: '/projects'
+        label: computed(() => $t('header.timeline')),
+        icon: 'mdi:timeline',
+        to: '/timeline'
     },
     {
-        label: computed( () => $t('header.contact')),
-        icon: 'mdi:contacts',
-        to: '/contact'
-    },
-    {
-        label: computed( () => $t('header.certificates')),
-        icon: 'mdi:certificate',
-        to: '/certificates'
-    },
-    {
-        label: computed( () => $t('header.skills')),
+        label: computed(() => $t('header.skills')),
         icon: 'mdi:school',
         to: '/skills'
     },
     {
-        label: computed( () => $t('header.timeline')),
-        icon: 'mdi:timeline',
-        to: '/timeline'
-    }
+        label: computed(() => $t('header.certificates')),
+        icon: 'mdi:certificate',
+        to: '/certificates'
+    },
+    {
+        label: computed(() => $t('header.projects')),
+        icon: 'mdi:briefcase',
+        to: '/projects'
+    },
+    {
+        label: computed(() => $t('header.contact')),
+        icon: 'mdi:contacts',
+        to: '/contact'
+    },
 ])
 
 const HeaderItemsFacade = computed(() => {
-    return isDesktop.value
+    return isLargeDesktop.value
         ? HeaderItemsDesktop
         : HeaderItemsMobile
 })
