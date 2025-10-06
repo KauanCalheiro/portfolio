5e37dc6d54b6b15cf6e3f0c94f7616ed4897ef60
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-10-06 14:26:34 +0100

    feat: add language skill

---
+++ app/components/skill/language.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+export interface Language {
+    name: string;
+    level: string;
+    icon_flag: string;
+}
+
+interface SkillLanguageProps {
+    language: Language;
+}
+
+const { language } = defineProps<SkillLanguageProps>();
+</script>
+
+<template>
+    <UButton
+        variant="soft"
+        class="h-full p-4 flex flex-col justify-between gap-2 cursor-default"
+    >
+        <UIcon
+            :name="language.icon_flag"
+            size="40"
+        />
+        <span class="text-sm text-center text-highlighted font-semibold">
+            {{ $t(language.name) }}
+        </span>
+        <span class="text-xs text-center text-muted">
+            {{ $t(language.level) }}
+        </span>
+    </UButton>
+</template>
---
+++ app/composables/useLanguages.ts
@@ -0,0 +1,12 @@
+export const useLanguages = () => [
+    {
+        name: 'skills.items.language.items.portuguese.name',
+        level: 'skills.items.language.items.portuguese.level',
+        icon_flag: 'emojione-v1:flag-for-brazil'
+    },
+    {
+        name: 'skills.items.language.items.english.name',
+        level: 'skills.items.language.items.english.level',
+        icon_flag: 'emojione-v1:flag-for-united-states'
+    }
+]
