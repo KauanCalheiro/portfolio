4989bfa422a2e6e477582a59dc3358bd68e203a6
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-10-06 14:27:47 +0100

    feat: add technologies skill

---
+++ app/components/skill/technology.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface SkillTechnologyProps {
+    icon: string;
+}
+
+const { icon } = defineProps<SkillTechnologyProps>();
+</script>
+
+<template>
+    <UButton
+        class="flex justify-center items-center p-4 cursor-default"
+        variant="soft"
+    >
+        <UIcon
+            :name="icon"
+            size="50"
+        />
+    </UButton>
+</template>
