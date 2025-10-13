c4623858c2fa3ad6cfb1e7bad4384a4ef64e09d8
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-10-13 23:42:11 +0100

    feat: add toast suport

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+import type { ToasterProps } from '@nuxt/ui';
 import * as locales from '@nuxt/ui/locale'
 
 const { locale } = useI18n()
+
+const toaster = ref<ToasterProps>({
+    position: 'bottom-center',
+    expand: false,
+})
 </script>
 
 <template>
   <ClientOnly>
-    <UApp :locale="locales[locale]">
+    <UApp
+      :toaster="toaster"
+      :locale="locales[locale]"
+    >
       <div class="bg-default" data-vaul-drawer-wrapper>
         <NuxtLayout>
           <NuxtPage />
